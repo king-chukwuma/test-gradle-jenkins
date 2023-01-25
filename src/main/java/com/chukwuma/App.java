fe3fe5ac496acc030690d2e0cf303ed45f4ef9a3
king-chukwuma <ohuabunwachukwuma@gmail.com>
2023-01-25 06:22:52 +0100

    first commit

---
+++ src/main/java/com/chukwuma/App.java
@@ -0,0 +1,10 @@
+package com.chukwuma;
+
+import com.google.common.collect.ImmutableList;
+
+public class App {
+    public static void main(String[] args) {
+        ImmutableList<String> of = ImmutableList.of("Hello");
+        System.out.println(of);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.chukwuma {
+    requires com.google.common;
+}
